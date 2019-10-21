9dd3fb67d0b1a3b224c593159e6a51405302d90c
Sharat Chandra <sharat.chandraj@gmail.com>
2019-10-21 23:57:01 +0000

    Added unit tests to improve coverage

--- rottenpotatoes/spec/movies_controller_spec.rb
+++ rottenpotatoes/spec/movies_controller_spec.rb
@@ -1,25 +1,71 @@
 require 'rails_helper'
 
 describe MoviesController, type: :controller do
-  describe "#search_director" do
-    context "When the specified movie has a director" do
-      it "should find movies with the same director" do
-        @movie_id = "1234"
-        @movie = double('fake_movie', :director => 'James Cameron')
-        expect(Movie).to receive(:find).with(@movie_id).and_return(@movie)
-        expect(@movie).to receive(:similar_movies)
-        get :director, :id => @movie_id
-        expect(response).to render_template(:director)
-      end
-    end
-    context "When the specified movie has no director" do
-      it "should redirect to the movies page" do
-        @movie_id = "1234" 
-        @movie = double('fake_movie').as_null_object
-        expect(Movie).to receive(:find).with(@movie_id).and_return(@movie)
-        get :director, :id => @movie_id
-        expect(response).to redirect_to(movies_path)
-      end
-    end
+  describe 'similar movies' do
+        before :each do
+            Movie.create!(:title => "Star Wars", :rating => "PG", :director => "George Lucas", :release_date => "1977-05-25")
+            Movie.create!(:title => "Blade Runner ", :rating => "PG", :director => "Ridley Scott", :release_date => "1982-06-25")
+            Movie.create!(:title => "Alien", :rating => "R", :director => "", :release_date => "1979-05-25")
+            Movie.create!(:title => "THX-1138", :rating => "R", :director => "George Lucas", :release_date => "1971-03-11")
+        end
+
+        it 'checks director for  Stars Wars' do
+            mov = Movie.find_by_title("Star Wars")
+            get :search_director, {:id => mov}
+            expect(assigns(:movies)).to eq(Movie.where(director: "George Lucas"))
+        end
+        it 'renders to similar movies template' do
+            mov = Movie.find_by_title("Star Wars")
+            get :search_director, {:id => mov}
+            expect(response).to render_template("search_director")
+        end
+        it 'redirects to the index when a director is not present' do
+            mov = Movie.find_by_title("Alien")
+            get :search_director, {:id => mov}
+            expect(flash[:notice]).to eq("\'#{mov.title}\' has no director info.")
+            expect(response).to redirect_to(root_url)
+        end
   end
+  describe "delete" do
+        before :each do
+            Movie.create!(:title => "what what", :rating => "PG", :director => "who cares", :release_date => "2019-01-1")
+            @total_movies = Movie.all.count
+        end
+
+      it "Should destroy a movie" do
+        
+        mov = Movie.find_by_title("what what")
+        get :destroy, :id=> mov
+
+        after_deleted = Movie.all.count
+        expect(flash[:notice]).to eq("Movie '#{mov.title}' deleted.")
+        expect(response).to redirect_to(movies_path)
+        expect(@total_movies-1).to eq(after_deleted)
+      end
+
+    end
+
+
+    describe "sort" do
+        before :each do
+            Movie.create!(:title => "Star Wars", :rating => "PG", :director => "George Lucas", :release_date => "1977-05-25")
+            Movie.create!(:title => "Blade Runner ", :rating => "PG", :director => "Ridley Scott", :release_date => "1982-06-25")
+            Movie.create!(:title => "Alien", :rating => "R", :director => "", :release_date => "1979-05-25")
+            Movie.create!(:title => "THX-1138", :rating => "R", :director => "George Lucas", :release_date => "1971-03-11")
+        end
+
+      it "sorts based on title" do
+        get :index, :sort=> 'title'
+      end
+
+      it "sorts based on date" do
+        get :index, :sort=> 'release_date'
+      end
+
+      it "shows all movies without the sort" do
+        get :index
+      end
+
+    end
+  
 end
